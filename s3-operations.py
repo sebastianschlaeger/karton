5a4b73976c2d36e5ba3b895851691a48b3edd190
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-07-24 16:22:02 +0200

    Update s3-operations.py

--- s3-operations.py
+++ s3-operations.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from datetime import datetime, timedelta
-from s3_utils import get_s3_fs
+import s3fs
+import streamlit as st
+
+def get_s3_fs():
+    return s3fs.S3FileSystem(
+        key=st.secrets["aws"]["AWS_ACCESS_KEY_ID"],
+        secret=st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"],
+        client_kwargs={
+            'region_name': st.secrets["aws"]["AWS_DEFAULT_REGION"]
+        }
+    )
 
 def save_unallocated_orders(unallocated_orders):
     s3 = get_s3_fs()
