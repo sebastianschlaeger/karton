8dae8728244219b8ab471e2059a530f098380c8c
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-09-04 10:53:53 +0200

    Update s3_operations.py

--- s3_operations.py
+++ s3_operations.py
@@ -17,17 +17,27 @@ def get_s3_fs():
 def save_unallocated_orders(unallocated_orders):
     s3 = get_s3_fs()
     bucket_name = st.secrets['aws']['S3_BUCKET_NAME']
-    filename = f"unallocated_orders_{datetime.now().strftime('%Y-%m-%d')}.json"
+    filename = "unallocated_orders.json"
     full_path = f"{bucket_name}/{filename}"
     
+    existing_orders = []
+    if s3.exists(full_path):
+        with s3.open(full_path, 'r') as f:
+            existing_orders = json.load(f)
+    
+    # Füge neue unzuordenbare Bestellungen hinzu
+    existing_orders.extend(unallocated_orders)
+    
+    # Entferne Duplikate basierend auf der Bestellnummer
+    unique_orders = {order['order_number']: order for order in existing_orders}
+    
     with s3.open(full_path, 'w') as f:
-        json.dump(unallocated_orders, f)
+        json.dump(list(unique_orders.values()), f)
 
 def get_unallocated_orders():
     s3 = get_s3_fs()
     bucket_name = st.secrets['aws']['S3_BUCKET_NAME']
-    today = datetime.now().date()
-    filename = f"unallocated_orders_{today.strftime('%Y-%m-%d')}.json"
+    filename = "unallocated_orders.json"
     full_path = f"{bucket_name}/{filename}"
     
     if s3.exists(full_path):
@@ -35,7 +45,7 @@ def get_unallocated_orders():
             return json.load(f)
     else:
         return []
-        
+
 def get_summary_data():
     try:
         s3 = get_s3_fs()
