dcb825bfdf442344203553d6a9d51ebeae3ab7eb
Max Mustermann <max.mustermann@example.com>
2024-09-05 10:11:43 +0200

    Ihre Commit-Nachricht

--- box_allocation.py
+++ box_allocation.py
@@ -5,18 +5,18 @@ def safe_float(value):
         return 0.0
 
 def allocate_box(total_weight):
-    if total_weight <= 500:
-        return '3001', 'Weight <= 500g'
-    elif total_weight <= 1000:
-        return '3002', '500g < Weight <= 1000g'
-    elif total_weight <= 2000:
-        return '3003', '1000g < Weight <= 2000g'
-    elif total_weight <= 5000:
-        return '3004', '2000g < Weight <= 5000g'
-    elif total_weight <= 10000:
-        return '3005', '5000g < Weight <= 10000g'
-    elif total_weight <= 20000:
-        return '3006', '10000g < Weight <= 20000g'
+    if total_weight <= 1800:
+        return '3001', 'Weight <= 1800g'
+    elif total_weight <= 3000:
+        return '3002', '1800g < Weight <= 3000g'
+    elif total_weight <= 5300:
+        return '3003', '3000g < Weight <= 5300g'
+    elif total_weight <= 10200:
+        return '3004', '5300g < Weight <= 10200g'
+    elif total_weight <= 20200:
+        return '3005', '10200g < Weight <= 20200g'
+    elif total_weight <= 31500:
+        return '3006', '20200g < Weight <= 31500g'
     else:
         return '3008', 'Weight > 20000g'
 
